af3f5dca087df481c8b1433eda7f18237e70a437
Jags <jagadeeshmurali@gmail.com>
2021-06-12 08:59:08 +0530

    Sample Test File

--- currency-exchange-service/src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
+++ currency-exchange-service/src/test/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class CurrencyExchangeServiceApplicationTests {
+	
+	@Mock
+	CurrencyExchangeRepository repo;
 
 	@Test
-	void contextLoads() {
+	void retrieveExchangeValue() {
+		CurrencyExchange currEx = new CurrencyExchange();
+		currEx.setConversionMultiple(new BigDecimal(100));
+		Mockito.when(repo.findByFromAndTo(Mockito.anyString(), Mockito.anyString()))
+				.thenReturn(currEx);
+		CurrencyExchange ex = repo.findByFromAndTo("USD", "INR");
+		double actualRes = ex.getConversionMultiple().doubleValue();
+		assertEquals(100, actualRes);
 	}
 
 }
